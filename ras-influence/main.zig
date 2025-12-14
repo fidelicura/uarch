58dd07f3d958f7fcf47ccc4c4d6020eefd557d8f
Kirill Bezuglyi <fidelicura@gmail.com>
2025-12-14 19:32:46 +0300

    just a quick git push

--- ras-influence/main.zig
+++ ras-influence/main.zig
@@ -4,36 +4,6 @@ const linux = std.os.linux;
 const task_type = *const volatile fn (usize) callconv(.c) void;
 const task_ptr: task_type = @ptrCast(&task);
 
-// noinline fn task(depth: usize) callconv(.c) void {
-//     // asm volatile (
-//     //     \\ testq %%rdi, %%rdi
-//     //     \\ je 1f
-//     //     \\ pushq %%rax
-//     //     \\ decq %%rdi
-//     //     \\ callq main.task
-//     //     \\ addq $8, %%rsp
-//     //     \\ 1:
-//     //     :
-//     //     : [depth] "{rdi}" (depth),
-//     //     : .{ .rax = true });
-
-//     // asm volatile (
-//     //     \\ testq %%rdi, %%rdi
-//     //     \\ je 3f
-//     //     \\ pushq %%rax
-//     //     \\ decq %%rdi
-//     //     \\ testq $1, %%rdi
-//     //     \\ jz 1f
-//     //     \\ callq main.task
-//     //     \\ jmp 2f
-//     //     \\ 1: callq main.task
-//     //     \\ 2: addq $8, %%rsp
-//     //     \\ 3:
-//     //     :
-//     //     : [depth] "{rdi}" (depth),
-//     //     : .{ .rax = true });
-// }
-
 noinline fn task(depth: usize) callconv(.c) void {
     if (depth > 1) @call(.never_tail, &task, .{depth - 1});
 }
@@ -51,13 +21,14 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
-    if (args.len != 4) {
-        std.debug.print("Usage: {s} <used_core> <call_depth> <call_amount>\n", .{args[0]});
+    if (args.len != 5) {
+        std.debug.print("Usage: {s} <used_core> <try_count> <call_depth> <call_amount>\n", .{args[0]});
         return error.InvalidArguments;
     }
     const used_core = try std.fmt.parseInt(i32, args[1], 10);
-    const call_depth = try std.fmt.parseInt(usize, args[2], 10);
-    const call_amount = try std.fmt.parseInt(usize, args[3], 10);
+    const try_count = try std.fmt.parseInt(usize, args[2], 10);
+    const call_depth = try std.fmt.parseInt(usize, args[3], 10);
+    const call_amount = try std.fmt.parseInt(usize, args[4], 10);
 
     // STEP: setup task queue.
 
@@ -83,11 +54,6 @@ pub fn main() !void {
         .config = 0xC9, // <https://github.com/torvalds/linux/blob/v6.17-rc7/tools/perf/pmu-events/arch/x86/amdzen4/branch.json#L47-L51>
         .flags = .{ .exclude_kernel = true, .exclude_hv = true },
     };
-    var btb_event = linux.perf_event_attr{
-        .type = .RAW,
-        .config = 0x91, // <https://github.com/torvalds/linux/blob/v6.17-rc7/tools/perf/pmu-events/arch/x86/amdzen4/branch.json#L12-L16>
-        .flags = .{ .exclude_kernel = true, .exclude_hv = true },
-    };
 
     // STEP: setup perf descriptors.
 
@@ -96,8 +62,6 @@ pub fn main() !void {
     const fd_misses: linux.fd_t = @intCast(linux.perf_event_open(&misses_event, 0, used_core, -1, linux.PERF.FLAG.FD_NO_GROUP));
     std.debug.assert(fd_misses >= 0);
     const fd_ras: linux.fd_t = @intCast(linux.perf_event_open(&ras_event, 0, used_core, -1, linux.PERF.FLAG.FD_NO_GROUP));
-    std.debug.assert(fd_misses >= 0);
-    const fd_btb: linux.fd_t = @intCast(linux.perf_event_open(&btb_event, 0, used_core, -1, linux.PERF.FLAG.FD_NO_GROUP));
     std.debug.assert(fd_misses >= 0);
 
     // STEP: reset perf metrics.
@@ -108,8 +72,6 @@ pub fn main() !void {
     std.debug.assert(return_value == 0);
     return_value = linux.ioctl(fd_ras, linux.PERF.EVENT_IOC.RESET, 0);
     std.debug.assert(return_value == 0);
-    return_value = linux.ioctl(fd_btb, linux.PERF.EVENT_IOC.RESET, 0);
-    std.debug.assert(return_value == 0);
 
     // STEP: disable perf temporarily.
 
@@ -118,8 +80,6 @@ pub fn main() !void {
     return_value = linux.ioctl(fd_branches, linux.PERF.EVENT_IOC.DISABLE, 0);
     std.debug.assert(return_value == 0);
     return_value = linux.ioctl(fd_ras, linux.PERF.EVENT_IOC.DISABLE, 0);
-    std.debug.assert(return_value == 0);
-    return_value = linux.ioctl(fd_btb, linux.PERF.EVENT_IOC.DISABLE, 0);
     std.debug.assert(return_value == 0);
 
     // STEP: lock address space.
@@ -131,58 +91,56 @@ pub fn main() !void {
 
     for (task_queue) |task_entry| @call(.never_tail, task_entry, .{call_depth});
 
-    // STEP: enable perf metrics.
+    // STEP: start tasks benchmarking.
 
-    return_value = linux.ioctl(fd_misses, linux.PERF.EVENT_IOC.ENABLE, 0);
-    std.debug.assert(return_value == 0);
-    return_value = linux.ioctl(fd_branches, linux.PERF.EVENT_IOC.ENABLE, 0);
-    std.debug.assert(return_value == 0);
-    return_value = linux.ioctl(fd_ras, linux.PERF.EVENT_IOC.ENABLE, 0);
-    std.debug.assert(return_value == 0);
-    return_value = linux.ioctl(fd_btb, linux.PERF.EVENT_IOC.ENABLE, 0);
-    std.debug.assert(return_value == 0);
+    std.debug.print("try_number,total_branches,branch_misses,miss_rate,ras_misses\n", .{});
+    for (1..try_count + 1) |try_number| {
+        // STEP: enable perf metrics.
 
-    // STEP: perform tasks benchmarking.
+        return_value = linux.ioctl(fd_misses, linux.PERF.EVENT_IOC.ENABLE, 0);
+        std.debug.assert(return_value == 0);
+        return_value = linux.ioctl(fd_branches, linux.PERF.EVENT_IOC.ENABLE, 0);
+        std.debug.assert(return_value == 0);
+        return_value = linux.ioctl(fd_ras, linux.PERF.EVENT_IOC.ENABLE, 0);
+        std.debug.assert(return_value == 0);
 
-    for (task_queue) |task_entry| @call(.never_tail, task_entry, .{call_depth});
+        // STEP: perform tasks benchmarking.
 
-    // STEP: disable perf metrics.
+        for (task_queue) |task_entry| @call(.never_tail, task_entry, .{call_depth});
 
-    return_value = linux.ioctl(fd_misses, linux.PERF.EVENT_IOC.DISABLE, 0);
-    std.debug.assert(return_value == 0);
-    return_value = linux.ioctl(fd_branches, linux.PERF.EVENT_IOC.DISABLE, 0);
-    std.debug.assert(return_value == 0);
-    return_value = linux.ioctl(fd_ras, linux.PERF.EVENT_IOC.DISABLE, 0);
-    std.debug.assert(return_value == 0);
-    return_value = linux.ioctl(fd_btb, linux.PERF.EVENT_IOC.DISABLE, 0);
-    std.debug.assert(return_value == 0);
+        // STEP: disable perf metrics.
 
-    // STEP: parse perf metrics.
+        return_value = linux.ioctl(fd_misses, linux.PERF.EVENT_IOC.DISABLE, 0);
+        std.debug.assert(return_value == 0);
+        return_value = linux.ioctl(fd_branches, linux.PERF.EVENT_IOC.DISABLE, 0);
+        std.debug.assert(return_value == 0);
+        return_value = linux.ioctl(fd_ras, linux.PERF.EVENT_IOC.DISABLE, 0);
+        std.debug.assert(return_value == 0);
 
-    var branch_instructions: usize = 0;
-    var branch_misses: usize = 0;
-    var ras_misses: usize = 0;
-    var btb_misses: usize = 0;
+        // STEP: parse perf metrics.
 
-    return_value = linux.read(@intCast(fd_branches), std.mem.asBytes(&branch_instructions), @sizeOf(usize));
-    std.debug.assert(return_value <= @sizeOf(usize));
-    return_value = linux.read(@intCast(fd_misses), std.mem.asBytes(&branch_misses), @sizeOf(usize));
-    std.debug.assert(return_value <= @sizeOf(usize));
-    return_value = linux.read(@intCast(fd_ras), std.mem.asBytes(&ras_misses), @sizeOf(usize));
-    std.debug.assert(return_value <= @sizeOf(usize));
-    return_value = linux.read(@intCast(fd_btb), std.mem.asBytes(&btb_misses), @sizeOf(usize));
-    std.debug.assert(return_value <= @sizeOf(usize));
+        var branch_instructions: usize = 0;
+        var branch_misses: usize = 0;
+        var ras_misses: usize = 0;
 
-    // STEP: calculate rate metrics.
+        return_value = linux.read(@intCast(fd_branches), std.mem.asBytes(&branch_instructions), @sizeOf(usize));
+        std.debug.assert(return_value <= @sizeOf(usize));
+        return_value = linux.read(@intCast(fd_misses), std.mem.asBytes(&branch_misses), @sizeOf(usize));
+        std.debug.assert(return_value <= @sizeOf(usize));
+        return_value = linux.read(@intCast(fd_ras), std.mem.asBytes(&ras_misses), @sizeOf(usize));
+        std.debug.assert(return_value <= @sizeOf(usize));
 
-    const count_branches: f64 = @floatFromInt(branch_instructions);
-    const count_misses: f64 = @floatFromInt(branch_misses);
-    const miss_rate = count_misses / count_branches * 100.0;
+        // STEP: calculate rate metrics.
 
-    // STEP: print benchmarking results.
+        const count_branches: f64 = @floatFromInt(branch_instructions);
+        const count_misses: f64 = @floatFromInt(branch_misses);
+        const miss_rate = count_misses / count_branches * 100.0;
 
-    std.debug.print(
-        "branches: {d}, missed: {d}, rate: {d:.2}, RAS missed: {d}, BTB missed: {d}\n",
-        .{ branch_instructions, branch_misses, miss_rate, ras_misses, btb_misses },
-    );
+        // STEP: print benchmarking results.
+
+        std.debug.print(
+            "{d},{d},{d},{d:.2},{d}\n",
+            .{ try_number, branch_instructions, branch_misses, miss_rate, ras_misses },
+        );
+    }
 }
